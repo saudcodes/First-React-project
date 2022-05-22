e418c65b2b25ffca1a288a228c4e7c3c978d39fd
Saudcodes <saudstunts@gmail.com>
2022-05-22 04:39:13 +0400

    form complete

--- my-app/src/components/NewForm.js
+++ my-app/src/components/NewForm.js
@@ -2,19 +2,31 @@ import React, { useState } from "react";
 
 const NewForm = () => {
     const [text, setText] = useState("default")
+
+    const convert = () => {
+        let a = text.toUpperCase()
+        setText(a)
+    }
+
+    const handleChange = (event) => {
+        setText(event.target.value)
+
+    }
+
     return (
         <>
             <div className="container">
                 <form class="was-validated">
                     <div className="mt-5">
                         <label htmlFor="validationTextarea" className="form-label">Textarea</label>
-                        <textarea value={text} className="form-control  is-invalid" rows="4" id="validationTextarea" placeholder="Required example textarea" required></textarea>
+                        <textarea value={text} onChange={handleChange} className="form-control  is-invalid" rows="4" id="validationTextarea" placeholder="Required example textarea" required></textarea>
                         <div className="invalid-feedback">
                             Please enter a message in the textarea.
                         </div>
-                        <button className="btn btn-primary" type="submit">Submit form</button>
+
                     </div>
                 </form>
+                <button className="btn btn-primary" onClick={convert} type="submit">Submit form</button>
             </div>
         </>
     )
